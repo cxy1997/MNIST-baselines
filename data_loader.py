41fdada7d97801f62516202e98e8bba5f58a0dee
cxy1997 <1023764859@qq.com>
2018-06-01 23:49:56 +0800

    Add dataset

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 # data loader for mnist dataset
 # the path is based on root dictory of mnist_project
 
+import os
 import numpy as np
 from PIL import Image
 
@@ -8,45 +9,26 @@ class MnistLoader(object):
     DATA_NUM = (60000, 10000)       # The number of figures
     FIG_W = 45                      # width of each figure
 
-    def __init__(self, fix=True, path='./data/'):
+    def __init__(self, flatten=False, data_path='data'):
         '''
-        :param path: the path to mnist dataset
-        '''
-
-        self.data_train, self.label_train = self._load(path + 'mnist_train/mnist_train_', 0)
-        self.data_test, self.label_test = self._load(path + 'mnist_test/mnist_test_', 1)
-
-        if fix:
-            self._fix()
-
-    def _load(self, path, mode=0):
-        '''
-        :param path: data path
-        :param mode: 0 - train; 1 - test
+        :param data_path: the path to mnist dataset
         '''
 
-        # load from files
-        data = np.fromfile(path + 'data', dtype=np.uint8)
-        label = np.fromfile(path + 'label', dtype=np.uint8)
+        self.data_train = np.concatenate([np.load(os.path.join(data_path, 'mnist_train', 'mnist_train_data_part1.npy')), np.load(os.path.join(data_path, 'mnist_train', 'mnist_train_data_part2.npy'))], axis=0)
+        self.label_train = np.load(os.path.join(data_path, 'mnist_train', 'mnist_train_label.npy'))
+        self.data_test = np.load(os.path.join(data_path, 'mnist_test', 'mnist_test_data.npy'))
+        self.label_test = np.load(os.path.join(data_path, 'mnist_test', 'mnist_test_label.npy'))
 
-        # reshape the matrix
-        data = data.reshape(self.DATA_NUM[mode], self.FIG_W, self.FIG_W)
-
-        return data, label
-
-    def _fix(self):
-        # flatten the data
-        self.data_train = self.data_train.reshape(self.data_train.shape[0], self.FIG_W * self.FIG_W)
-        self.data_test = self.data_test.reshape(self.data_test.shape[0], self.FIG_W * self.FIG_W)
-
-        # change label into one-hot vector
-        self.label_train = np.eye(10)[self.label_train.reshape(-1)]
-        self.label_test = np.eye(10)[self.label_test.reshape(-1)]
+        if flatten:
+            self.data_train = self.data_train.reshape(self.data_train.shape[0], -1)
+            self.data_test = self.data_test.reshape(self.data_test.shape[0], -1)
 
     def demo(self):
         # show the structure of data & label
-        print(self.data_train.shape)
-        print(self.label_train.shape)
+        print('Train data:', self.data_train.shape)
+        print('Train labels:', self.label_train.shape)
+        print('Test data:', self.data_test.shape)
+        print('Test labels:', self.label_test.shape)
 
         # choose a random index
         ind = np.random.randint(0, self.DATA_NUM[0])
