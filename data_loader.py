06a0fe730467a99bf599f8bbdd7dca837b3bfcf6
Shin <542782758@qq.com>
2018-06-03 16:49:25 +0800

    test performance on 28*28 mnist

--- data_loader.py
+++ data_loader.py
@@ -9,8 +9,8 @@ class MnistLoader(object):
     DATA_SIZE = (60000, 10000)       # number of figures
     FIG_W = 45                      # width of each figure
     CLASSES = 10                    # number of classes
-    MEAN = 14.48175858436214                    # number of classes
-    STD = 54.43364982731134                    # number of classes
+    MEAN = 14.48175858436214
+    STD = 54.43364982731134
 
     def __init__(self, flatten=False, data_path='data'):
         '''
@@ -52,6 +52,75 @@ class MnistLoader(object):
         plt.show()
         # im.save("demo.png")
 
+class NormalMnistLoader(object):
+    DATA_SIZE = (60000, 10000)       # number of figures
+    FIG_W = 28                      # width of each figure
+    CLASSES = 10                    # number of classes
+    # MEAN = 14.48175858436214
+    # STD = 54.43364982731134
+
+    def __init__(self, flatten=False, data_path='data'):
+        '''
+        :param data_path: the path to mnist dataset
+        '''
+
+        self.data_train = np.load(os.path.join(data_path, 'mnist_train_normal', 'mnist_train_data.npy')).astype(np.float32)
+        self.label_train = np.load(os.path.join(data_path, 'mnist_train_normal', 'mnist_train_label.npy')).astype(np.int64)
+        self.data_test = np.load(os.path.join(data_path, 'mnist_test_normal', 'mnist_test_data.npy')).astype(np.float32)
+        self.label_test = np.load(os.path.join(data_path, 'mnist_test_normal', 'mnist_test_label.npy')).astype(np.int64)
+
+        # self.mean = 0 # self.MEAN # self.data_train.mean()
+        # self.std = 255.0 # self.STD # self.data_train.std()
+        # self.data_train = (self.data_train - self.mean) / self.std
+        # self.data_test = (self.data_test - self.mean) / self.std
+
+        if flatten:
+            self.data_train = self.data_train.reshape(self.data_train.shape[0], -1)
+            self.data_test = self.data_test.reshape(self.data_test.shape[0], -1)
+
+    def byte2npy(self):
+        with open('data/mnist_train_normal/train-images.idx3-ubyte', 'rb') as f:
+            f.read(16)
+            train_data = np.fromfile(f, dtype=np.uint8)
+        with open('data/mnist_train_normal/train-labels.idx1-ubyte', 'rb') as f:
+            f.read(8)
+            train_label = np.fromfile(f, dtype=np.uint8)
+        with open('data/mnist_test_normal/t10k-images.idx3-ubyte', 'rb') as f:
+            f.read(16)
+            test_data = np.fromfile(f, dtype=np.uint8)
+        with open('data/mnist_test_normal/t10k-labels.idx1-ubyte', 'rb') as f:
+            f.read(8)
+            test_label = np.fromfile(f, dtype=np.uint8)
+
+        train_data = train_data.reshape(60000, self.FIG_W * self.FIG_W)
+        test_data = test_data.reshape(10000, self.FIG_W * self.FIG_W)
+
+        np.save('data/mnist_train_normal/mnist_train_data.npy', train_data)
+        np.save('data/mnist_train_normal/mnist_train_label.npy', train_label)
+        np.save('data/mnist_test_normal/mnist_test_data.npy', test_data)
+        np.save('data/mnist_test_normal/mnist_test_label.npy', test_label)
+
+    def demo(self):
+        # show the structure of data & label
+        print('Train data:', self.data_train.shape)
+        print('Train labels:', self.label_train.shape)
+        print('Test data:', self.data_test.shape)
+        print('Test labels:', self.label_test.shape)
+        # print('mean:', self.mean)
+        # print('std:', self.std)
+
+        # choose a random index
+        ind = np.random.randint(0, self.DATA_SIZE[0])
+
+        # print the index and label
+        print("index: ", ind)
+        print("label: ", self.label_train[ind])
+
+        # save the figure
+        plt.imshow(self.data_train[ind].reshape(self.FIG_W, self.FIG_W))
+        plt.show()
+        # im.save("demo.png")
+
 if __name__ == '__main__':
-    loader = MnistLoader()
+    loader = NormalMnistLoader()
     loader.demo()
---
+++ main_test.py
@@ -0,0 +1,50 @@
+from __future__ import division, print_function
+import os
+import argparse
+import json
+import numpy as np
+import torch
+from pydoc import locate
+from data_loader import NormalMnistLoader
+from utils import init_dir, show_config, setup_logger, latest_model
+
+parser = argparse.ArgumentParser(description="MNIST classifiers")
+parser.add_argument("--method", type=str, default="drop_connect_test")
+
+parser.add_argument("--resume", action="store_true")
+parser.add_argument("--config-dir", type=str, default="config")
+parser.add_argument("--data-dir", type=str, default="data")
+parser.add_argument("--model-dir", type=str, default="trained_models")
+parser.add_argument("--log-dir", type=str, default="logs")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    with open(os.path.join(args.config_dir, "%s.json" % args.method)) as f:
+        config = json.load(f)
+    for arg in vars(args):
+        config[arg] = getattr(args, arg)
+    show_config(config)
+    config["last_epoch"] = 0
+
+    init_dir(args.model_dir)
+    init_dir(args.log_dir)
+    init_dir(os.path.join(args.model_dir, args.method))
+    np.random.seed(config["seed"])
+    torch.manual_seed(config["seed"])
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+    data = NormalMnistLoader(flatten=config["flatten"], data_path=args.data_dir)
+    model = locate("models.%s.%s" % (args.method, config["model_name"]))()
+    if args.resume:
+        model_path, config["last_epoch"] = latest_model(args.model_dir, args.method)
+        print("Loading latest model from %s" % model_path)
+        model.load_state_dict(torch.load(model_path))
+    if torch.cuda.is_available():
+        model = model.cuda()
+    model.train()
+    optimizer = locate("torch.optim.%s" % config["optimizer"])(model.parameters(), lr=config["lr"],
+                                                               weight_decay=config["weight_decay"])
+    logger = setup_logger(args.method, os.path.join(args.log_dir, "%s.log" % args.method), resume=args.resume)
+
+    train = locate("trainers.%s_trainer.train" % args.method)
+    train(data, model, optimizer, logger, config)
